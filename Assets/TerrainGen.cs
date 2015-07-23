8961ab876c8e1fbe553d738b730f836efbd40088
hellenator <m.ispanki@yahoo.com>
2015-07-23 17:08:31 +0200

    added rarity to ore generation according to ore type/value

--- Assets/TerrainGen.cs
+++ Assets/TerrainGen.cs
@@ -12,10 +12,12 @@ public class TerrainGen{
             FileStream file = new FileStream("./file.txt", FileMode.Create);
             StreamWriter wf = new StreamWriter(file);
 
-            int gen = 200;
+            int gen = 50;
+            float[,] h2 = new float[,] { {0.3f, 15f}, {0.25f,20f}, {0.2f,22f}, {0.15f,25f}, {0.1f,30f}, {0.07f,33f}, {0.03f,37f} };
             System.Random rand = new System.Random(seed);
             
-            Tile[,] map = new Tile[200, 200];
+            Tile[,] map = new Tile[50, 50];
+            Tile initial;
 
             bool t = false;
 
@@ -32,9 +34,12 @@ public class TerrainGen{
             {
                 for (int j = 0; j < gen; j++)
                 {
+                    initial = new Ore(rand, i, j);
+                    h = h2[(int)initial.type,0];
+                    reduce = h2[(int)initial.type, 1];
                     if (rand.Next(0, 100) <= h && !t)
                     {
-                        map[i, j] = new Ore(rand, i, j);
+                        map[i, j] = initial;
 
                         map = ((Ore)map[i, j]).Spread(map, h, rand, reduce);
 
