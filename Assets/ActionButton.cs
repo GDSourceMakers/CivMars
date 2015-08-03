2b68603688841d1c23a3538d484f0459160f52c6
dpeter99 <dpeter99@gmail.com>
2015-08-03 09:02:22 +0200

    Added the secound half of the last update

---
+++ Assets/ActionButton.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+using UnityEngine.UI;
+
+public class ActionButton : MonoBehaviour
+{
+
+
+    public float countdown;
+    public float fullcount;
+
+    public GameController GameCon;
+
+    GameObject playergo;
+
+    public Button buttonscript;
+
+
+    public float defaultYPos;
+    public float defaultXPos;
+
+    public RectTransform maskRect;
+    public string action = "Mine";
+
+
+    public bool ActionRuned;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameCon = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+
+        countdown = 0;
+
+        buttonscript = gameObject.GetComponent<Button>();
+
+        if (buttonscript == null)
+            Debug.LogError("Can't find Button Script");
+
+        maskRect = gameObject.transform.FindChild("Mask").GetComponent<RectTransform>();
+
+
+        defaultYPos = maskRect.localPosition.y;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown > 0)
+        {
+            buttonscript.interactable = false;
+            
+            
+
+            maskRect.localPosition = new Vector2(maskRect.localPosition.x, Mathf.Lerp(defaultYPos + maskRect.rect.height, defaultYPos, (countdown / fullcount)));
+            ActionRuned = false;
+            GameCon.playerclass.GetComponent<Rigidbody2D>().isKinematic = true;
+            countdown -= Time.deltaTime;
+        }
+        else
+        {
+            buttonscript.interactable = true;
+            maskRect.position = new Vector2(maskRect.position.x, defaultYPos + maskRect.rect.height);
+
+            if (!ActionRuned)
+            {
+
+                MethodInfo m = GameCon.playerclass.GetComponent<Player>().GetType().GetMethod(action);
+                m.Invoke(GameCon.playerclass.GetComponent<Player>(), null);
+                ActionRuned = true;
+                GameCon.playerclass.GetComponent<Rigidbody2D>().isKinematic = false;
+            }
+        }
+
+    }
+
+    public void Action(float time, string functionname)
+    {
+        if (countdown <= 0)
+        {
+            action = functionname;
+            countdown = time;
+            fullcount = time;
+        }
+    }
+
+
+}
---
+++ Assets/Language.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Language
+{
+
+
+    static public string[,] Names =
+    {
+    {"CoalOre","Coal Ore"},
+    {"StoneOre","Stone"},
+    {"SandOre","Sand"},
+    {"IronOre","Iron Ore"},
+    {"UraniumOre","Uran Ore"},
+    {"CoalOre","Coal Ore"},
+    {"CoalOre","Coal Ore"}
+    };
+
+    public static string Get(System.Object itemObject)
+    {
+        string name = "Unlocalized Name";
+
+        for (int i = 0; i < 7; i++)
+	    {
+        
+            if (Names[i,0] == itemObject.GetType().ToString())
+            {
+                name = Names[i, 1];
+            }
+        }
+
+        if (name == "Unlocalized Name")
+            Debug.LogError("Cant find: " + itemObject.GetType().ToString());
+        return name;
+    }
+}
