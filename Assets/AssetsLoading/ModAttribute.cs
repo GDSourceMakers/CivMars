4a2097c110e877093ead2bf2b779a9d3f6826282
dpeter99 <dpeter99@gmail.com>
2015-12-04 16:54:32 +0100

    - Gui update  - FONTS

---
+++ Assets/AssetsLoading/ModAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[AttributeUsage(AttributeTargets.Class)]
+class ModAttribute : Attribute
+{
+
+}
+
+
+[AttributeUsage(AttributeTargets.Method)]
+class ModInitAttribute : Attribute
+{
+
+}
--- Assets/Camera/CameraChase.cs
+++ Assets/Camera/CameraChase.cs
@@ -159,8 +159,8 @@ public class CameraChase : MonoBehaviour
 			float xPos = chase.transform.position.x;
 			float yPos = -1 * chase.transform.position.y;
 
-			float xCameraH = CameraScript.orthographicSize * Screen.width / Screen.height;
-			float yCameraH = CameraScript.orthographicSize;
+			float xCameraH = CameraScript.orthographicSize * Screen.width / Screen.height * (1 - CameraScript.rect.y);
+			float yCameraH = CameraScript.orthographicSize * (1-CameraScript.rect.y);
 
 			//transform.position = new Vector3(Mathf.Clamp(xPos, xCameraH, GameCon.map.mapHeight - xCameraH),-1 * Mathf.Clamp(-1*yPos, yCameraH, GameCon.map.mapHeight - yCameraH), transform.position.z);
 			transform.position = new Vector3(
